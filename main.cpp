2c97bbbed617bf60357cb06ac7c9ddfdd3de50a2
Daniel Henrique <138157786+danielhenriquee@users.noreply.github.com>
2025-08-05 22:03:13 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <locale.h>
 #include <time.h>
-#define TAM 9
+#define SIZE 9
 using namespace std;
 
 #ifdef _WIN32
@@ -14,26 +14,26 @@ using namespace std;
 
 int main() {
     #ifdef _WIN32
-        system("chcp 65001 > nul"); // Para suportar acentos no terminal do Windows
+        system("chcp 65001 > nul"); // To support accents in Windows terminal
     #endif
     setlocale(LC_ALL, "pt_BR.UTF-8");
     srand(time(NULL));
 
-    int menu, i, j, aleatorio, ganhou;
-    int cont = 0, il; // cont é o contador de jogadas // il é varíavel utilizada para o índice de linhas da matriz jogo
-    int aux, auxi, auxj; // Variáveis utilizadas na hora de criar a matriz jogo e depois para jogar o sudoku
+    int menu, i, j, random, won;
+    int counter = 0, ri; // Move counter // Row index of game matrix
+    int value, inputRow, inputCol; // Variables used to play
 
-    int principal[TAM][TAM] = { 4, 9, 5, 2, 8, 7, 3, 6, 1, 
-                                7, 2, 8, 6, 1, 3, 4, 9, 5, 
-                                3, 6, 1, 9, 5, 4, 7, 2, 8, 
-                                6, 5, 3, 8, 4, 9, 2, 1, 7, 
-                                9, 8, 4, 1, 7, 2, 6, 5, 3, 
-                                2, 1, 7, 5, 3, 6, 9, 8, 4, 
-                                1, 3, 2, 4, 6, 5, 8, 7, 9, 
-                                5, 4, 6, 7, 9, 8, 1, 3, 2, 
-                                8, 7, 9, 3, 2, 1, 5, 4, 6}; // Matriz principal
-    int gabarito[TAM][TAM]; // Matriz gabarito
-    int jogo[TAM][TAM]; // Matriz em que será o jogo
+    int solution[SIZE][SIZE] = { 4, 9, 5, 2, 8, 7, 3, 6, 1, 
+                                  7, 2, 8, 6, 1, 3, 4, 9, 5, 
+                                  3, 6, 1, 9, 5, 4, 7, 2, 8, 
+                                  6, 5, 3, 8, 4, 9, 2, 1, 7, 
+                                  9, 8, 4, 1, 7, 2, 6, 5, 3, 
+                                  2, 1, 7, 5, 3, 6, 9, 8, 4, 
+                                  1, 3, 2, 4, 6, 5, 8, 7, 9, 
+                                  5, 4, 6, 7, 9, 8, 1, 3, 2, 
+                                  8, 7, 9, 3, 2, 1, 5, 4, 6}; // Main matrix
+    int key[SIZE][SIZE]; // Key matrix
+    int game[SIZE][SIZE]; // Game matrix
     
     do {
         cout << endl;
@@ -43,112 +43,112 @@ int main() {
                 "                                                          ╚════██║██║   ██║██║  ██║██║   ██║██╔═██╗ ██║   ██║\n"
                 "                                                          ███████║╚██████╔╝██████╔╝╚██████╔╝██║  ██╗╚██████╔╝\n"
                 "                                                          ╚══════╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ \n\n\n\n";
-        cout << "                                                                            1.  Jogar\n\n\n"
-                "                                                                            2.  SOBRE\n\n\n"
-                "                                                                            3.  SAIR\n\n\n\n";
+        cout << "                                                                            1.  PLAY\n\n\n"
+                "                                                                            2.  ABOUT\n\n\n"
+                "                                                                            3.  EXIT\n\n\n\n";
         cin >> menu;
 
         switch (menu) {
-        case 1: // Jogar
+        case 1: // Play
             system(CLEAR);
-            aleatorio = rand() % 4 + 1; // Gera um número pseudo-aleatório de 1 a 4 para definir a matriz gabarito
-
-            if (aleatorio == 1) { // Gera a matriz gabarito sem modificação
-                for (i = 0; i < TAM; i++) {
-                    for (j = 0; j < TAM; j++) {
-                        gabarito[i][j] = principal[i][j];
+            random = rand() % 4 + 1; // Generates a pseudo-random number from 1 to 4 to define the template matrix
+            
+            if (random == 1) { // Generate key matriz (without changes)
+                for (i = 0; i < SIZE; i++) {
+                    for (j = 0; j < SIZE; j++) {
+                        key[i][j] = solution[i][j];
                     }
                 }
-            } else if (aleatorio == 2) { // Gera a matriz gabarito transposta
-                for (i = 0; i < TAM; i++) {
-                    for (j = 0; j < TAM; j++) {
-                        gabarito[i][j] = principal[j][i];
+            } else if (random == 2) { // Generate key matriz (transposed)
+                for (i = 0; i < SIZE; i++) {
+                    for (j = 0; j < SIZE; j++) {
+                        key[i][j] = solution[j][i];
                     }
                 }
-            } else if (aleatorio == 3) { // Gera a matriz gabarito invertida por linha
-                for (i = 0; i < TAM; i++) {
-                    for (j = 0; j < TAM; j++) {
-                        gabarito[8 - i][j] = principal[i][j];
+            } else if (random == 3) { // Generate key matriz (inverted by rows)
+                for (i = 0; i < SIZE; i++) {
+                    for (j = 0; j < SIZE; j++) {
+                        key[8 - i][j] = solution[i][j];
                     }
                 }
-            } else if (aleatorio == 4) { // Gera a matriz gabarito invertida por coluna
-                for (i = 0; i < TAM; i++) {
-                    for (j = 0; j < TAM; j++) {
-                        gabarito[i][8 - j] = principal[i][j];
+            } else if (random == 4) { // Generate key matriz (inverted by columns)
+                for (i = 0; i < SIZE; i++) {
+                    for (j = 0; j < SIZE; j++) {
+                        key[i][8 - j] = solution[i][j];
                     }
                 }
             }
     
-            for (i = 0; i < TAM; i++) {
-                for (j = 0; j < TAM; j++) {
-                    jogo[i][j] = gabarito[i][j]; // Copia a matriz gabarito para a do jogo
+            for (i = 0; i < SIZE; i++) {
+                for (j = 0; j < SIZE; j++) {
+                    game[i][j] = key [i][j]; // Copy key matrix to game matrix
                 }
             }
     
-            for (aux = 0; aux < 40;) {
-                auxi = rand() % 9; // Gera um número pseudo-aleatório de 0 a 8 para definir coordenada linha
-                auxj = rand() % 9; // Gera um número pseudo-aleatório de 0 a 8 para definir coordenada coluna
-                if (jogo[auxi][auxj] > 0) { // Confere se o valor já foi zerado
-                    aux++;
+            for (value  = 0; value  < 40;) {
+                inputRow = rand() % 9; // Generates a pseudo-random number from 0 to 8 to define row coordinate
+                inputCol = rand() % 9; // Generates a pseudo-random number from 0 to 8 to define column coordinate
+                if (game[inputRow][inputCol] > 0) { // Check if the value has already been reset
+                    value ++;
                 }
-                jogo[auxi][auxj] = 0; // Zera o valor
+                game[inputRow][inputCol] = 0; // Reset value
             }
     
-            ganhou = 40;
-            do { // Começo do jogo em si, quando o usuário começa a interagir
-                il = 1;
-                cout << "Contador de jogadas: " << cont << endl << endl;
-                cout << " 1 2 3  4 5 6  7 8 9  \n"; // Printa o índice de colunas antes do loop que printa a matriz jogo
-                cout << "╔══════╦══════╦═════╗\n"; // Printa essa borda antes do loop que printa a matriz jogo
-                for (i = 0; i < TAM; i++) {
-                    if (i == 3 || i == 6 ) // Printa essa linha após a 3ª e a 6ª linha da matriz
+            won = 40;
+            do { // Game starts
+                ri = 1;
+                cout << "Move counter: " << counter << endl << endl;
+                cout << " 1 2 3  4 5 6  7 8 9  \n"; // Prints the column index before the loop that prints the game matrix
+                cout << "╔══════╦══════╦═════╗\n"; // Print this edge before the loop that prints the game matrix
+                for (i = 0; i < SIZE; i++) {
+                    if (i == 3 || i == 6 ) // Print this row after the 3rd and 6th lines of the matrix
                         cout << "╠══════╬══════╬═════╣\n";
-                    for (j = 0; j<TAM; j++) {
-                        if (j % 3 == 0) { // Printa essa coluna a cada 3 colunas
+                    for (j = 0; j < SIZE; j++) {
+                        if (j % 3 == 0) { // Print this row after the 3rd and 6th row of the matrix
                             cout << "║"; 
                         }
-                        if (jogo[i][j] == 0 && j != 8) { // Se o valor for 0, printa vazio com espaço depois
+                        if (jogo[i][j] == 0 && j != 8) { // If the value is 0, print empty with space after
                             cout << "  ";
-                        } else if (jogo[i][j] == 0 && j == 8) { // Se o valor for 0 na última coluna, printa vazio sem espaço depois
+                        } else if (game[i][j] == 0 && j == 8) { // If the value is 0 in the last column, print empty with no space after it
                             cout << " ";
-                        } else if (jogo[i][j] != 0 && j < 8) { // Se tiver valor e não for a último coluna, printa o valor com espaço depois
-                            cout <<jogo[i][j] << " ";
-                        } else { // Printa o valor sem o espaço depois (última coluna)
-                            cout << jogo[i][j]; 
+                        } else if (game[i][j] != 0 && j < 8) { // If it has a value and it is not the last column, print the value with a space after it
+                            cout << game[i][j] << " ";
+                        } else { // Print the value without the space after it (last column)
+                            cout << game[i][j]; 
                         }
                     }
-                    cout << "║ " << il << endl; // Após cada fim da linha, printa a ║ como borda e pula a linha
-                    il++;
+                    cout << "║ " << ri << endl; // After each end of row, print a ║ as border and skip the row
+                    ri++;
                 }
-                cout << "╚══════╩══════╩═════╝\n\n"; // Após o loop que printa a matriz jogo, printa essa linha no final
+                cout << "╚══════╩══════╩═════╝\n\n"; // After the loop that prints the game matrix, print this row at the end
                 
-                cout << "Digite a linha que quer preencher...";
-                cin >> auxi;
-                cout << "Digite a coluna que quer preencher...";
-                cin >> auxj;
-                cout << "Digite o valor que quer preencher...";
-                cin >> aux;
+                cout << "Enter the row you want to fill...";
+                cin >> inputRow;
+                cout << "Enter the column you want to fill...";
+                cin >> inputCol;
+                cout << "Enter the value you want to fill...";
+                cin >> value;
                 system(CLEAR);
             
-                if (aux == gabarito[auxi - 1][auxj - 1] && jogo[auxi - 1][auxj - 1] == 0) { // Confere se o valor é válido em posição não preenchida
-                    jogo[auxi - 1][auxj - 1] = aux;
-                    ganhou--; // Esse valor começa em 40 que é a quantidade de valores que falta descobrir, ele vai diminuindo, quando chega a zero ganha
-                    cout << "Você acertou um valor!\n\n";
-                } else if ((auxi < 1 || auxi > 9 || auxj < 1 || auxj > 9) && (aux < 1 || aux > 9)) { // Confere se a posição é inválida E o valor é inválido
-                    cout << "Você digitou uma posição inválida e também um valor inválido! Digite apenas números naturais de 1 até 9.\n\n";
-                } else if (auxi < 1 || auxi > 9 || auxj < 1 || auxj > 9) { // Confere se a posição é inválida
-                    cout << "Você digitou uma posição inválida! Digite um número natural de 1 até 9.\n\n";
-                } else if (jogo[auxi - 1][auxj - 1] > 0) { // Confere se a posição já foi preenchida
-                    cout << "Esta posição já foi preenchida!\n\n";
-                } else if (aux < 1 || aux > 9) { // Confere se o valor é inválido
-                    cout << "Você digitou um valor inválido! Digite um número natural de 1 até 9.\n\n";
-                } else if (aux != gabarito[auxi - 1][auxj - 1] && jogo[auxi - 1][auxj - 1] == 0) { // Confere se o valor é incorreto
-                    cout << "Você digitou um valor incorreto!\n\n";
+                if (value == key[inputRow - 1][inputCol - 1] && game[inputRow - 1][inputCol - 1] == 0) { // Check if the value is valid in an unfilled position
+                    game[inputRow - 1][inputCol - 1] = value;
+                    won--; // This value starts at 40, which is the number of values left to discover, it decreases, and when it reaches zero, you win.
+                    cout << "You got a value right!\n\n";
+                } else if ((inputRow < 1 || inputRow > 9 || inputCol < 1 || inputCol > 9) && (value < 1 || value > 9)) { // Check if the position is invalid && the value is invalid
+                    cout << "You entered an invalid position and an invalid value! Enter only natural numbers from 1 to 9.\n\n";
+                } else if (inputRow < 1 || inputRow > 9 || inputCol < 1 || inputCol > 9) { // Check if the position is invalid
+                    cout << "You entered an invalid position! Enter a natural number from 1 to 9.\n\n";
+                } else if (game[inputRow - 1][inputCol - 1] > 0) { // Check if the position has already been filled
+                    cout << "This position has already been filled!\n\n";
+                } else if (value < 1 || value > 9) { // Check if the value is invalid
+                    cout << "You entered an invalid value! Please enter a natural number from 1 to 9.\n\n";
+                } else if (game != key[inputRow - 1][inputCol - 1] && game[inputRow - 1][inputCol - 1] == 0) { // Check if the value is incorrect
+                    cout << "You entered an incorrect value!\n\n";
                 }
             
-                cont++; // Aumenta a variável contador a cada tentativa
+                counter++; // Increase the counter variable with each attempt
             
-            } while (ganhou > 0);
+            } while (won > 0);
     
             system(CLEAR);
             cout << "                                                 ██╗   ██╗ ██████╗ ██╗   ██╗    ██╗    ██╗██╗███╗   ██╗  ██╗██╗██╗\n"
@@ -158,34 +158,34 @@ int main() {
                     "                                                    ██║   ╚██████╔╝╚██████╔╝    ╚███╔███╔╝██║██║ ╚████║  ██╗██╗██╗\n"
                     "                                                    ╚═╝    ╚═════╝  ╚═════╝      ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝  ╚═╝╚═╝╚═╝\n\n\n\n";
     
-            cout << "Você precisou de " << cont << " jogadas para completar o sudoku.\n";
-            cout << "Digite qualquer tecla para voltar ao menu...\n";
-            cont = 0; // Zera o contador de jogadas
+            cout << "You needed " << cont << " moves to complete the sudoku.\n";
+            cout << "Type any character to return to the menu...";
+            cont = 0; // Reset move counter
             cin.ignore();
             cin.get();
             system(CLEAR);
             break;
         
-        case 2: // Sobre
+        case 2: // About
             system(CLEAR);
-            cout << "\nDesenvolvedores:    Daniel Henrique da Silva\n"
-                    "                    Gabriel Henrique da Silva\n"
-                    "                    Jorge Luiz Siemann Pereira\n"
-                    "                    Lucas dos Santos Luckow\n\n"
-                    "Professor:          Prof. Rafael Ballotin Martins\n"
-                    "Matéria:            Algoritmos e Programação\n"
-                    "Junho/2023\n\n";
-            cout << "Digite qualquer tecla para voltar ao menu\n";
+            cout << "\nDevelopers:    Daniel Henrique da Silva\n"
+                    "               Gabriel Henrique da Silva\n"
+                    "               Jorge Luiz Siemann Pereira\n"
+                    "               Lucas dos Santos Luckow\n"
+                    "Professor:     Prof. Rafael Ballotin Martins\n"
+                    "Course:        Algorithms and Programming I\n"
+                    "June/2023\n\n";
+            cout << "Type any character to return to the menu...";
             cin.ignore();
             cin.get();
             system(CLEAR);
             break;
 
-        case 3: // Sair
+        case 3: // Exit
             break;
-        } // Fecha o switch
+        }
 
-        system(CLEAR); // Limpa tudo antes de voltar ao menu
-    } while (menu != 3); // Fecha o loop do jogo
+        system(CLEAR);
+    } while (menu != 3);
     return 0;
 }
